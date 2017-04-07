cf3e2c3bed410c041bc61f9ced1c00d76c82acaf
cleanAndrew <andrew.clean.code@gmail.com>
2017-04-07 15:03:09 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,37 @@
+
+# ActiveCollab API
+
+## Install
+> npm install activecollabjs
+
+## Using
+> ac.api(method, [options], [callback]);
+>
+> Use request options. More details [here](https://www.npmjs.com/package/request#requestoptions-callback)
+
+
+```javascript
+const ac = require('activecollabjs');
+
+ac.init({
+  username: 'member@activecollab.com',
+  password: 'Easy to remember, hard to guess',
+  client_name: 'My Awesome App',
+  client_vendor: 'ACME Inc',
+  host: 'http://my.selfhost.com/activecollab/'
+}, (err, response) => {
+  let options = {
+    json: true,
+    method: 'POST',
+    headers: [/* You can add custom headers */],
+    body: {
+      name: 'Task #1',
+      labels: ['New','Deferred']
+    }
+  };
+
+  ac.api('/projects/1/tasks', options, (err, response) => {
+    console.log(response);
+  });
+});
+```
---
+++ index.js
@@ -0,0 +1,101 @@
+
+const _ = require('lodash');
+const path = require('path');
+const request = require('request');
+
+/**
+ * @constructor ActiveCollabJS
+ */
+const ActiveCollabJS = function() {};
+
+
+/**
+ * Init API - get api token by username && password
+ *
+ * @typedef {Object} AuthInfo
+ * @property {String} username      :: User email (login)
+ * @property {String} password      :: User password
+ * @property {String} client_name   :: Application name
+ * @property {String} client_vendor :: Application owner
+ * @property {String} [host=Cloud]  :: ActiveCollab host
+ *
+ * @callback issueToken~response
+ * @param {Object} error              :: Request error
+ * @param {Object} response           :: Response JSON object
+ *
+ * @param {AuthInfo} params           :: initial parameters
+ * @param {Function} callback         :: issueToken~response
+ * @return {issueToken~response}      :: callback
+ */
+ActiveCollabJS.prototype.init = function(params, callback) {
+  // Save properties
+  _.assign(this, { host: 'https://my.activecollab.com/api/v1/' }, params);
+
+  let parameters = {
+    json: true,
+    body: params,
+    method: 'POST'
+  };
+
+  this.api('/issue-token', parameters, (err, res) => {
+    if (err) {
+      return callback(err);
+    }
+
+    if (res.is_ok) {
+      this.token = res.token;
+    } else {
+      // Auth error
+      return callback(res);
+    }
+
+    callback(null, res);
+  });
+};
+
+
+/**
+ * Request wrapper
+ * @param  {String}   method          :: official api method path
+ * @param  {Object}   params          :: api call parameters
+ * @param  {Function} callback        :: issueToken~response
+ * @return {issueToken~response}      :: callback
+ */
+ActiveCollabJS.prototype.__request = function(method, params, callback) {
+  let url = this.host + path.join('api/v1/', method);
+
+  request(url, params, (err, res, body) => callback(err, body));
+};
+
+
+/**
+ * Api call wrapper
+ * @param  {String}   method          :: official api method path
+ * @param  {Object}   params          :: api call parameters
+ * @param  {Function} callback        :: issueToken~response
+ * @return {issueToken~response}      :: callback
+ */
+ActiveCollabJS.prototype.api = function(method, params = {}, callback) {
+  if (typeof params === 'function') {
+    [params, callback] = [{}, params];
+  }
+
+  if (typeof callback !== 'function') {
+    callback = () => {};
+    console.info('Callback not found!');
+  }
+
+  let headers = _.assign(params.headers || {}, {
+    'X-Angie-AuthApiToken': this.token
+  });
+
+  let settings = _.assign(params, {
+    method: params.method || 'GET',
+    headers: headers
+  });
+
+  this.__request(method, settings, callback);
+};
+
+
+module.exports = ActiveCollabJS;
